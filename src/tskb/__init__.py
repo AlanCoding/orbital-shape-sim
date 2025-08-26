88a61821153f29506b546772343da14d0befdb66
Alan Rominger <arominge@redhat.com>
2025-08-26 09:14:17 -0400

    docs: add neural net controller usage guide

--- src/tskb/__init__.py
+++ src/tskb/__init__.py
@@ -2,7 +2,12 @@
 
 from .env import Environment
 from .barbell import DualBarbell, Q_of_barbell
-from .controller import BangBangController, PassiveController, make_controller
+from .controller import (
+    BangBangController,
+    PassiveController,
+    NeuralNetController,
+    make_controller,
+)
 from .integrate import run_simulation
 from . import diagnostics
 
@@ -12,6 +17,7 @@ __all__ = [
     "Q_of_barbell",
     "BangBangController",
     "PassiveController",
+    "NeuralNetController",
     "make_controller",
     "run_simulation",
     "diagnostics",
